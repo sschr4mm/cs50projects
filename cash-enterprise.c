8bbb4c5eb4aeb31839ce43fd97800b0804858903
Sydney Schramm <40502946+sschr4mm@users.noreply.github.com>
2024-02-17 13:02:51 -0500

    Perfected enterprise approach with switches
    I got this as scalable as far as I can think of without using arrays. Pretty happy about it. The only other path apparent to me involves using modulo but not sure how much it would improve this code's design.

--- cash-enterprise.c
+++ cash-enterprise.c
@@ -1,117 +1,133 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Global constant and variable definitions
 const int quarter = 25;
 const int dime = 10;
 const int nickel = 5;
 const int penny = 1;
 
-int numQuarters;
-int numDimes;
-int numNickels;
-int numPennys;
-int totQuarters;
-int totDimes;
-int totNickels;
-int totPennys;
+int numQuarters = 0;
+int numDimes = 0;
+int numNickels = 0;
+int numPennys = 0;
 
-int rem;
+int totQuarters = 0;
+int totDimes = 0;
+int totNickels = 0;
+int totPennys = 0;
+
+int numCoins;
+int totCoins;
+int rem; // Remainder to keep track of remaining total after each function
+int changeOwed;
+
+//////////////////////////////////
+bool checkCoins(int coin) // Check if a coin is required
+{
+    if ((rem / coin) >= 1)
+    {
+        return true;
+    }
+    else
+        return false;
+}
+
+// Functions that calculate the number of coins, and their total sum.
+int calcNumCoins(coinType)
+{
+    numCoins = rem / coinType;
+    return numCoins;
+}
+int calcTotCoins(coinType)
+{
+    totCoins = (numCoins * coinType);
+    return totCoins;
+}
+
+// Process remaining amount
+int calcRem(remCoins)
+{
+    rem = rem - remCoins;
+    return rem;
+}
+
+// This function checks which coins and respective functions will be needed. If needed, will then return total # of coins, the sum
+// of the coins, and a working remainder total for downstream calculations.
+int calcCoins(int amountToCalculate)
+{
+    if (checkCoins(quarter) == true)
+    {
+        numQuarters = calcNumCoins(quarter);
+        totQuarters = calcTotCoins(quarter);
+        calcRem(totQuarters);
+    }
+
+    if (checkCoins(dime) == true)
+    {
+        numDimes = calcNumCoins(dime);
+        totDimes = calcTotCoins(dime);
+        calcRem(totDimes);
+    }
+
+    if (checkCoins(nickel) == true)
+    {
+        numNickels = calcNumCoins(nickel);
+        totNickels = calcTotCoins(nickel);
+        calcRem(totNickels);
+    }
+
+    if (checkCoins(penny) == true)
+    {
+        numPennys = calcNumCoins(penny);
+        totPennys = calcTotCoins(penny);
+        calcRem(totPennys);
+    }
+
+    changeOwed = numQuarters + numDimes + numNickels + numPennys;
+    return changeOwed;
+}
+
+void printChangeOwed()
+{
+    if (numQuarters != 0)
+    {
+        printf("%i x quarters = %i\n", numQuarters, totQuarters);
+    }
+
+    if (numDimes != 0)
+    {
+        printf("%i x dimes = %i\n", numDimes, totDimes);
+    }
+
+    if (numNickels != 0)
+    {
+        printf("%i x nickels = %i\n", numNickels, totNickels);
+    }
+
+    if (numPennys != 0)
+    {
+        printf("%i x pennys = %i\n", numPennys, totPennys);
+    }
+    printf("---------------\n= %i total # of coins\n", changeOwed);
+}
 //////////////////////////////////////
 int main(void)
 {
+    // function definitions
     int calcCoins(int amountToCalculate);
     int initialAmount;
-    int changeOwed;
 
+    // logic
     do
     {
         initialAmount = get_int("Change owed: ");
     }
     while (initialAmount < 0);
-    printf("---------------\n");
 
-    int coins = calcCoins(initialAmount);
+    rem = initialAmount; // remainder: used to keep track of the working total after each function calculation. Separate from
+                         // initialAmount so as not to corrupt the original value.
 
-    printf("%i x quarters = %i\n", numQuarters, totQuarters);
-    printf("%i x dimes = %i\n", numDimes, totDimes);
-    printf("%i x nickels = %i\n", numNickels, totNickels);
-    printf("%i x pennys = %i\n", numPennys, totPennys);
-    printf("---------------\n= %i total # of coins\n", coins);
-}
-
-// Functions to calculate the number of coins, and total sum of respective coins.
-int calcNumQuarters(int amountToCalculate)
-{
-    numQuarters = amountToCalculate / quarter;
-    return numQuarters;
-}
-int calcTotQuarters(int amountToCalculate)
-{
-    totQuarters = (numQuarters * quarter);
-    return totQuarters;
-}
-int calcNumDimes(int amountToCalculate)
-{
-    numDimes = rem / dime;
-    return numDimes;
-}
-int calcTotDimes(int amountToCalculate)
-{
-    totDimes = (numDimes * dime);
-    return totDimes;
-}
-int calcNumNickels(int amountToCalculate)
-{
-    numNickels = rem / nickel;
-    return numNickels;
-}
-int calcTotNickels(int amountToCalculate)
-{
-    totNickels = (numNickels * nickel);
-    return totNickels;
-}
-int calcNumPennys(int amountToCalculate)
-{
-    numPennys = rem / penny;
-    return numPennys;
-}
-int calcTotPennys(int amountToCalculate)
-{
-    totPennys = (numPennys * penny);
-    return totPennys;
-}
-
-// Process remaining amount
-int calcRem(int amountToCalculate, int totCoins)
-{
-    rem = amountToCalculate - totCoins;
-    return rem;
-}
-
-// This would have been useful for arrays/switch loops to save processing time for amounts like 1, 4, 5, 19, 51 where not all coins need to be processed.
-// int divideCoins(int amountToCalculate, int coin)
-// {
-//     return amountToCalculate / coin;
-// }
-
-// This function calls each function to get the total number of coins required. It also ensures other functions run to get the sum of the coins.
-int calcCoins(int amountToCalculate)
-{
-    calcNumQuarters(amountToCalculate);
-    calcTotQuarters(amountToCalculate);
-    calcRem(amountToCalculate, totQuarters);
-
-    calcNumDimes(rem);
-    calcTotDimes(rem);
-    calcRem(rem, totDimes);
-
-    calcNumNickels(rem);
-    calcTotNickels(rem);
-    calcRem(rem, totNickels);
-
-    calcNumPennys(rem);
-    calcTotPennys(rem);
-    calcRem(rem, totPennys);
-
-    return numQuarters + numDimes + numNickels + numPennys;
+    calcCoins(initialAmount);
+    printChangeOwed();
 }
